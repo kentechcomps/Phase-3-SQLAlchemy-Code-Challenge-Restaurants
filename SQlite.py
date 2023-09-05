9963065a3f96e4c19f57cf26ce386618e4cc05eb
user <mutukukennedy34@gmail.com>
2023-09-05 09:29:45 +0300

    fakermethod

--- SQlite.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from models import Base
-
-
-engine = create_engine('sqlite:///restaurant.db')
-
-# Create the tables in the database
-Base.metadata.create_all(engine)
--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Text, engine_from_config
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+from faker import Faker
 Base = declarative_base()
 
 class Restaurant(Base):
@@ -10,11 +10,22 @@ class Restaurant(Base):
     name = Column(String)
     price = Column(Integer)
 
+    def reviews(self):
+        return self.reviews
+    def customers(self):
+        return[review.customer for review in self.reviews]
+
 class Customer(Base):
     __tablename__ = 'customers'
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
+
+    def reviews(self):
+        return self.reviews
+    def restaurant(self):
+        return[review.restaurant for review in self.reviews]
+
 
 class Review(Base):
     __tablename__ = 'reviews'
@@ -27,14 +38,23 @@ class Review(Base):
     restaurant = relationship('Restaurant', backref='reviews')
     customer = relationship('Customer', backref='reviews')
 
+    def customer(self):
+        return self.customer
+    def restaurant(self):
+        return self.restaurant
+
 
     engine = create_engine('sqlite:///Restaurant.db')
+    Base.metadata.create_all(engine)
 
 # Create a session
 Session = sessionmaker(bind=engine_from_config)
 session = Session()
 
 # Seed data for restaurants
+fake = Faker()
+
+
 restaurant1 = Restaurant(name='Restaurant1', price=2)
 restaurant2 = Restaurant(name='Restaurant2', price=3)
 
@@ -43,7 +63,7 @@ customer1 = Customer(first_name='John', last_name='Doe')
 customer2 = Customer(first_name='Jane', last_name='Smith')
 
 # Add data to the session
-session.add_all([restaurant1, restaurant2, customer1, customer2])
+session.bulk_save_objects([restaurant1, restaurant2, customer1, customer2])
 
 # Commit the changes
 session.commit()
